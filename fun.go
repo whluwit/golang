a70a0983dc17b3322d28d45663682f2f6bfa7d50
jack <wanghailiang-0@163.com>
2016-04-22 16:24:55 +0800

    str

--- fun.go
+++ fun.go
@@ -2,10 +2,32 @@ package main
 
 import "fmt"
 
-var c, python, java bool
+type person struct {
+	name string
+	age  int
+}
 
+func Older(p1, p2 person) (person, int) {
+	if p1.age > p2.age {
+		return p1, p1.age - p2.age
+	}
+	return p2, p2.age - p1.age
+}
 func main() {
-	var i int
-	fmt.Println(i, c, python, java)
+	var tom person
+	tom.name, tom.age = "Tom", 18
+	bob := person{age: 25, name: "Bob"}
+	paul := person{"Paul", 43}
+
+	tb_Older, tb_diff := Older(tom, bob)
+	tp_Older, tp_diff := Older(tom, paul)
+	bp_Older, bp_diff := Older(bob, paul)
+
+	fmt.Println("of %s and %s , %S is older by %d years\n", tom.name, bob.name, tb_Older.name, tb_diff)
+
+	fmt.Println("of %s and %s , %S is older by %d years\n",
+		tom.name, paul.name, tp_Older.name, tp_diff)
+	fmt.Println("of %s and %s , %S is older by %d years\n",
+		bob.name, paul.name, bp_Older.name, bp_diff)
 
 }
